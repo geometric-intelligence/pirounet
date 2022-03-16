6f472e2ab11a24f8e806545865bfea0cbabfada7
Mathilde Papillon <papillon@umail.ucsb.edu>
2022-03-15 17:54:44 -0700

    added testing, with artifact production. fixed bug in architecture data shape

--- move/main.py
+++ move/main.py
@@ -14,11 +14,12 @@ from nn import *
 from config import *
 
 logging.info('Initialize WandB project')
-wandb.init(project="lstm_vae_valid", entity="mathildepapillon")
+
+wandb.init(project="lstm_vae", entity="mathildepapillon", settings=wandb.Settings(start_method="thread"))
 wandb.config = {
   "learning_rate": learning_rate,
   "epochs": epochs,
-  "batch_size": batchsize,
+  "batch_size": batch_size,
   "seq_len": seq_len
 }
 
@@ -41,11 +42,11 @@ if SERVER == 'pod':
 logging.info('Initialize model')
 my_encoder = LstmEncoder(input_features=3*53, h_features_loop=32, latent_dim=32).to(device)
 my_decoder = LstmDecoder(n_layers=2, output_features=3*53, h_features_loop=32, latent_dim=32, seq_len=128).to(device)
-model = LstmVAE().to(device)
+model = LstmVAE().to(device)  # LstmVAE(input_features, h_features_loop=...)
 
 logging.info('Load data')
 ds_all, ds_all_centered, datasets, datasets_centered, ds_counts = load_data()
-my_data = ds_all.reshape((ds_all.shape[0], -1))
+my_data = ds_all_centered.reshape((ds_all.shape[0], -1))
 
 logging.info('Make seq_data of shape [number of seq, seq_len, input_features]')
 seq_data = np.zeros((my_data.shape[0]-seq_len, seq_len, my_data.shape[1]))
@@ -53,19 +54,24 @@ for i in range((ds_all.shape[0]-seq_len)):
     seq_data[i] = my_data[i:i+seq_len]
 
 logging.info('Make training data and validing data')
-ten_perc = int(round(seq_data.shape[0]*0.1))
-eighty_perc = seq_data.shape[0] - (2*ten_perc)
-valid_ds = seq_data[:ten_perc, :, :]
-test_ds = seq_data[ten_perc:(2*ten_perc), :, :]
-train_ds = seq_data[eighty_perc:, :, :]
+five_perc = int(round(seq_data.shape[0]*0.05))
+ninety_perc = seq_data.shape[0] - (2*five_perc)
+valid_ds = seq_data[:five_perc, :, :]
+test_ds = seq_data[five_perc:(2*five_perc), :, :]
+train_ds = seq_data[ninety_perc:, :, :]
 
 logging.info('Make torch tensor in batches')
 data_train_torch = torch.utils.data.DataLoader(train_ds, 
-    batch_size=batchsize, num_workers=2)
+    batch_size=batch_size, num_workers=2)
 data_valid_torch = torch.utils.data.DataLoader(valid_ds, 
-    batch_size=batchsize, num_workers=2)
+    batch_size=batch_size, num_workers=2)
+data_test_torch = torch.utils.data.DataLoader(test_ds, 
+    batch_size=1, num_workers=2)
+
 
 logging.info('Train/validate and record loss')
 wandb.watch(model, get_loss, log="all", log_freq=100)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999))
-run_train(model, data_train_torch, data_valid_torch, get_loss, optimizer, epochs)
+run_train(model, data_train_torch, data_valid_torch, data_test_torch, get_loss, optimizer, epochs)
+
+wandb.finish()
