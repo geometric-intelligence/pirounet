dd28b7fac9e351ea1e3641bf4ba7488a05b1db02
Nina Miolane <nmiolane@gmail.com>
2022-04-01 20:06:35 -0700

    Update default config

--- move/default_config.py
+++ move/default_config.py
@@ -1,17 +1,18 @@
 import logging
+
 import torch
 
-#Set the configuration of the model 
-logging.info('Confirgure the run')
+# Set the configuration of the model
+logging.info("Confirgure the run")
 batch_size = 8
-learning_rate= 3e-4
+learning_rate = 3e-4
 epochs = 20
-seq_len=128
-negative_slope = 0 #LeakyRelu
+seq_len = 128
+negative_slope = 0  # LeakyRelu
 kl_weight = 0
 
-logging.info('Setup device')
+logging.info("Setup device")
 if torch.cuda.is_available():
-    device = torch.device('cuda')
+    device = torch.device("cuda")
 else:
-    device = torch.device('cpu')
+    device = torch.device("cpu")
