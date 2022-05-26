fd4ab8620c53c7aba42f8d9f52634ccba9fabe1f
Nina Miolane <nmiolane@harold.ece.ucsb.edu>
2022-05-26 00:51:46 +0000

    Add actor classifier

--- move/models/classifiers.py
+++ move/models/classifiers.py
@@ -34,7 +34,12 @@ class LinearClassifier(nn.Module):
         self.layers.append(nn.Linear(h_dim, label_dim))
 
     def forward(self, x):
-        """Perform forward pass of the linear classifier."""
+        """Perform forward pass of the linear classifier.
+
+        Parameters
+        ----------
+        x : array-like, shape=[batch_size, seq_len, input_dim]
+        """
 
         batch_size = x.shape[0]
         x = x.reshape((batch_size, -1)).float()
@@ -53,7 +58,7 @@ class LinearClassifier(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model=default_config.input_dim, dropout=0.1, max_len=5000):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):  # d_model
         super(PositionalEncoding, self).__init__()
 
         self.dropout = nn.Dropout(p=dropout)
@@ -116,111 +121,81 @@ class TransformerClassifier(PositionalEncoding):
 class ActorClassifier(torch.nn.Module):
     def __init__(
         self,
-        modeltype,
-        njoints,
-        nfeats,
-        num_frames,
-        num_classes,
-        translation,
-        pose_rep,
-        glob,
-        glob_rot,
-        latent_dim=256,
-        ff_size=1024,
-        num_layers=4,
-        num_heads=4,
+        seq_len,
+        label_dim,
+        embed_dim=256,
+        dim_feedforward=1024,
+        n_layers=4,
+        n_heads=4,
         dropout=0.1,
-        ablation=None,
         activation="gelu",
         **kargs
     ):
         super().__init__()
 
-        self.modeltype = modeltype
         self.njoints = njoints
         self.nfeats = nfeats
-        self.num_frames = num_frames
-        self.num_classes = num_classes
+        self.seq_len = seq_len
+        self.label_dim = label_dim
 
-        self.pose_rep = pose_rep
-        self.glob = glob
-        self.glob_rot = glob_rot
-        self.translation = translation
+        self.embed_dim = embed_dim
 
-        self.latent_dim = latent_dim
-
-        self.ff_size = ff_size
-        self.num_layers = num_layers
-        self.num_heads = num_heads
+        self.dim_feedforward = dim_feedforward
+        self.n_layers = n_layers
+        self.n_heads = n_heads
         self.dropout = dropout
 
-        self.ablation = ablation
         self.activation = activation
 
-        self.input_feats = self.njoints * self.nfeats
+        self.input_dim = self.njoints * self.nfeats
 
-        if self.ablation == "average_encoder":
-            self.mu_layer = nn.Linear(self.latent_dim, self.latent_dim)
-            self.sigma_layer = nn.Linear(self.latent_dim, self.latent_dim)
-        else:
-            self.muQuery = nn.Parameter(torch.randn(self.num_classes, self.latent_dim))
-            self.sigmaQuery = nn.Parameter(
-                torch.randn(self.num_classes, self.latent_dim)
-            )
+        self.mu_query = nn.Parameter(torch.randn(self.label_dim, self.embed_dim))
+        self.sigma_query = nn.Parameter(torch.randn(self.label_dim, self.embed_dim))
 
-        self.skelEmbedding = nn.Linear(self.input_feats, self.latent_dim)
+        self.embedding = nn.Linear(self.input_dim, self.embed_dim)
 
-        self.sequence_pos_encoder = PositionalEncoding(self.latent_dim, self.dropout)
+        self.sequence_pos_encoder = PositionalEncoding(self.embed_dim, self.dropout)
 
-        # self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-
-        seqTransEncoderLayer = nn.TransformerEncoderLayer(
-            d_model=self.latent_dim,
-            nhead=self.num_heads,
-            dim_feedforward=self.ff_size,
+        seq_transformer_encoder_layer = nn.TransformerEncoderLayer(
+            d_model=self.embed_dim,
+            nhead=self.n_heads,
+            dim_feedforward=self.dim_feedforward,
             dropout=self.dropout,
             activation=self.activation,
         )
-        self.seqTransEncoder = nn.TransformerEncoder(
-            seqTransEncoderLayer, num_layers=self.num_layers
+        self.seq_transformer_encoder = nn.TransformerEncoder(
+            seq_transformer_encoder_layer, n_layers=self.n_layers
         )
 
-    def forward(self, batch):
-        x, y, mask = batch["x"], batch["y"], batch["mask"]
-        bs, njoints, nfeats, nframes = x.shape
-        x = x.permute((3, 0, 1, 2)).reshape(nframes, bs, njoints * nfeats)
+    def forward(self, x):
+        """Perform forward pass of the linear classifier.
+
+        Parameters
+        ----------
+        x : array-like, shape=[batch_size, seq_len, input_dim]
+        """
+        _, seq_len, input_dim = x.shape
+        assert seq_len == self.seq_len
+        assert input_dim == self.input_dim
+
+        # Transforming to shape seq_len, batch_size, input_dim
+        # to match Actor's architecture
+        x = x.permute((1, 0, 2))
+        seq_len, _, input_dim = x.shape
+        assert seq_len == self.seq_len
+        assert input_dim == self.input_dim
 
         # embedding of the skeleton
-        x = self.skelEmbedding(x)
+        x = self.embedding(x)
 
-        # only for ablation / not used in the final model
-        if self.ablation == "average_encoder":
-            # add positional encoding
-            x = self.sequence_pos_encoder(x)
+        # adding the mu and sigma queries
+        xseq = torch.cat((self.mu_query[y][None], self.sigma_query[y][None], x), axis=0)
 
-            # transformer layers
-            final = self.seqTransEncoder(x, src_key_padding_mask=~mask)
-            # get the average of the output
-            z = final.mean(axis=0)
+        # add positional encoding
+        xseq = self.sequence_pos_encoder(xseq)
 
-            # extract mu and logvar
-            mu = self.mu_layer(z)
-            logvar = self.sigma_layer(z)
-        else:
-            # adding the mu and sigma queries
-            xseq = torch.cat(
-                (self.muQuery[y][None], self.sigmaQuery[y][None], x), axis=0
-            )
-
-            # add positional encoding
-            xseq = self.sequence_pos_encoder(xseq)
-
-            # create a bigger mask, to allow attend to mu and sigma
-            muandsigmaMask = torch.ones((bs, 2), dtype=bool, device=x.device)
-            maskseq = torch.cat((muandsigmaMask, mask), axis=1)
-
-            final = self.seqTransEncoder(xseq, src_key_padding_mask=~maskseq)
-            mu = final[0]
-            logvar = final[1]
+        final = self.seq_transformer_encoder(xseq)
+        mu = final[0]
+        logvar = final[1]
 
         return {"mu": mu, "logvar": logvar}
